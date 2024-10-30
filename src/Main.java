ef37b5d57f8094e0b66cc118f8d1a6dd385e42b6
Zoe Aspenns <aspennza@mail.uc.edu>
2024-10-30 12:22:51 -0400

    Task 1-8 drafts complete

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main
 {
@@ -6,8 +7,15 @@ public class Main
     {
         final int ARRAY_LENGTH = 100;
         Random rnd = new Random();
+        Scanner in = new Scanner(System.in);
         int sum = 0;
         double avg = 0;
+        int userValue = 0;
+        boolean foundUserValue1 = false;
+        boolean foundUserValue2 = false;
+        int userValueCounter = 0;
+        int minimum = 0;
+        int maximum = 0;
 
         //Task 1
         int[] dataPoints = new int[ARRAY_LENGTH];
@@ -34,6 +42,61 @@ public class Main
         System.out.println("The average of the data in the random array dataPoints is: " + avg + ".");
 
         //Task 5
+        userValue = SafeInput.getRangedInt(in, "Please input a number to search for in the dataPoints array", 1, 100);
 
+        //Task 6
+        for(int y = 0; y < dataPoints.length; y++)
+        {
+            if(dataPoints[y] == userValue)
+            {
+                foundUserValue1 = true;
+                userValueCounter++;
+                System.out.println("\nYour value of " + userValue + " was found at index " + y + ".");
+            }
+        }
+        if(!foundUserValue1)
+        {
+            System.out.println("\nYour value of " + userValue + " was not found in the data set.");
+        }
+        System.out.println("\nYour value of " + userValue + " was found " + userValueCounter + " times.");
+
+        //Task 7
+        userValue = SafeInput.getRangedInt(in, "Please input another number to search for in the dataPoints array", 1, 100);
+
+        for(int z = 0; z < dataPoints.length; z++)
+        {
+            if(dataPoints[z] == userValue)
+            {
+                foundUserValue2 = true;
+                System.out.println("\nYour value of " + userValue + " was found at index " + z + ".");
+                break;
+            }
+        }
+        if(!foundUserValue2)
+        {
+            System.out.println("\nYour value of " + userValue + " was not found in the data set.");
+        }
+
+        //Task 8
+        minimum = dataPoints[0];
+        maximum = dataPoints[0];
+
+        for(int a = 0; a < dataPoints.length; a++)
+        {
+            if(minimum > dataPoints[a])
+            {
+                minimum = dataPoints[a];
+            }
+
+            if(maximum < dataPoints[a])
+            {
+                maximum = dataPoints[a];
+            }
+        }
+        System.out.println("\nThe minimum value in the random array dataPoints is " + minimum + ".");
+        System.out.println("\nThe maximum value in the random array dataPoints is " + maximum + ".");
     }
+
+
+
 }
